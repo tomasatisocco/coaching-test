9c68aacbaf354246c804cefbb779182dcfc47048
Tomas Tisocco <tomastisocco97@gmail.com>
2023-05-05 01:41:38 -0300

    Create project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.coaching
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:coaching/app/app.dart';
+import 'package:coaching/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
