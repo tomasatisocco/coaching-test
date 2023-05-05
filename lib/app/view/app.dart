0701ccb55406b2dd5d384e4bd1aae4dcaaeaba54
Tomas Tisocco <tomastisocco97@gmail.com>
2023-05-05 01:57:48 -0300

    Add go router

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,13 +1,30 @@
 import 'package:coaching/counter/counter.dart';
 import 'package:coaching/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late GoRouter _router;
+
+  @override
+  void initState() {
+    _router = router(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerDelegate: _router.routerDelegate,
+      routeInformationParser: _router.routeInformationParser,
+      routeInformationProvider: _router.routeInformationProvider,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
@@ -16,7 +33,20 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+    );
+  }
+
+  GoRouter router(BuildContext context) {
+    return GoRouter(
+      initialLocation: '/counter',
+      routes: <GoRoute>[
+        GoRoute(
+          path: '/counter',
+          builder: (_, state) {
+            return const CounterPage();
+          },
+        ),
+      ],
     );
   }
 }
