fbc7c5d21b99b51b61e93223e2c5d50693e16292
Tomas Tisocco <tomastisocco97@gmail.com>
2023-05-06 18:56:39 -0300

    Update tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,15 @@
 import 'package:coaching/app/app.dart';
-import 'package:coaching/counter/counter.dart';
+import 'package:coaching/welcome/view/welcome_page.dart';
+import 'package:firestore_repository/firestore_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      await tester.pumpWidget(App(
+        firestoreRepository: FirestoreRepository.development(),
+      ));
+      expect(find.byType(WelcomePage), findsOneWidget);
     });
   });
 }
