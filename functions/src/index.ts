8854e6f44edd3d46bac4336d9a3e371ebf6c7456
Tomas Tisocco <tomastisocco97@gmail.com>
2023-05-22 08:29:12 -0300

    Add propper cover page and clean functions code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,7 @@ import * as admin from "firebase-admin";
 import * as nodemailer from "nodemailer";
 import * as dotenv from "dotenv";
 import {Storage} from "@google-cloud/storage";
-import {PassThrough} from "stream";
-import {PDFDocument} from "pdf-lib";
+import {PDFDocument, StandardFonts, rgb} from "pdf-lib";
 
 // Initialize Firebase Admin SDK
 admin.initializeApp();
@@ -19,10 +18,11 @@ const password = dotenv.config().parsed?.GMAIL_PASSWORD;
 const storage = new Storage();
 
 const bucketName = "gs://coaching-test-3c129.appspot.com/";
-const fileName = "development/testresult.pdf";
+const filePath = "development/testresult.pdf";
+const iconPath = "development/live_as_coach_icon.png";
+const imagePath = "development/live_as_coach_cover.png";
 
 const bucket = storage.bucket(bucketName);
-const defaultResult = bucket.file(fileName);
 
 // Initialize Nodemailer transport
 const transporter = nodemailer.createTransport({
@@ -33,70 +33,174 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Utility function to convert a readable stream to a buffer
-const streamToBufferAsync = (stream: PassThrough): Promise<Buffer> => {
-  return new Promise((resolve, reject) => {
-    const chunks: Buffer[] = [];
+/**
+ * This function creates the cover page of the PDF.
+ * @param {PDFDocument} pdfDoc The PDF document.
+ * @param {Uint8Array} iconBuffer The icon image buffer.
+ * @param {Uint8Array} imageBuffer The image buffer.
+ * @param {string} userName The user name.
+ * @param {string} date The date of the test.
+ */
+async function createCoverPage(
+  pdfDoc: PDFDocument,
+  iconBuffer: Uint8Array,
+  imageBuffer: Uint8Array,
+  userName: string,
+  date: string,
+): Promise<void> {
+  // Add a new cover page
+  const coverPage = pdfDoc.insertPage(0);
+  const {width, height} = coverPage.getSize();
 
-    stream.on("data", (chunk) => {
-      chunks.push(chunk);
-    });
+  const [coverWidth, coverHeight, coverX, coverY] = [width, height * 0.7, 0, 0];
+  const [iconWidth, iconHeight, iconX, iconY] = [
+    100, 80, width / 2 - 50, height - 100,
+  ];
 
-    stream.on("end", () => {
-      resolve(Buffer.concat(chunks));
-    });
+  // Add the icon image at the top
+  const iconImage = await pdfDoc.embedPng(iconBuffer);
+  coverPage.drawImage(
+    iconImage,
+    {x: iconX, y: iconY, width: iconWidth, height: iconHeight},
+  );
 
-    stream.on("error", (error) => {
-      reject(error);
-    });
-  });
-};
+  // Add the text below the icon
+  const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  const italicFont = await pdfDoc.embedFont(StandardFonts.TimesRomanItalic);
+  const titleFontSize = 24;
+  const subTitleFontSize = 12;
+  const title = "Test para el desarrollo profesional";
+  const subTitle = "Informe";
+  const titleWidth = italicFont.widthOfTextAtSize(title, titleFontSize);
+  const subTitleWidth = font.widthOfTextAtSize(subTitle, titleFontSize);
+  const nameWidth = font.widthOfTextAtSize(userName, subTitleFontSize);
+  const dateWidth = font.widthOfTextAtSize(date, subTitleFontSize);
+  coverPage.drawText(
+    title,
+    {
+      x: (width - titleWidth) / 2,
+      y: height - 136,
+      size: titleFontSize,
+      color: rgb(0, 0, 0),
+      font: italicFont,
+    }
+  );
+  coverPage.drawText(
+    subTitle,
+    {
+      x: (width - subTitleWidth) / 2,
+      y: height - 162,
+      size: titleFontSize,
+      color: rgb(0, 0, 0),
+      font: font,
+    }
+  );
+  coverPage.drawText(
+    userName,
+    {
+      x: (width - nameWidth) / 2,
+      y: height - 186,
+      size: subTitleFontSize,
+      color: rgb(0, 0, 0),
+      font: font,
+    }
+  );
+  coverPage.drawText(
+    date,
+    {
+      x: (width - dateWidth) / 2,
+      y: height - 210,
+      size: subTitleFontSize,
+      color: rgb(0, 0, 0),
+      font: font,
+    }
+  );
+
+  // Add the image to fill the rest of the cover page
+  const image = await pdfDoc.embedPng(imageBuffer);
+  coverPage.drawImage(
+    image,
+    {x: coverX, y: coverY, width: coverWidth, height: coverHeight},
+  );
+  functions.logger.log("Cover page created");
+}
+
+/**
+ * This function adds the results to the pdf.
+ * @param {PDFDocument} pdfDoc The PDF document.
+ * @param {string} userId The document id.
+ */
+async function addResultPDF(
+  pdfDoc: PDFDocument,
+  userId: string,
+): Promise<void> {
+  // Load the source PDF
+  const resultsPath = "development/UsersResults/"+userId+".pdf";
+  const sourcePDFData = await bucket.file(resultsPath).download();
+  const sourcePDFBuffer = Buffer.from(sourcePDFData[0]);
+  const sourcePDFDoc = await PDFDocument.load(sourcePDFBuffer);
+
+  // Copy pages from the source PDF to the target PDF
+  const copiedPages = await pdfDoc.copyPages(
+    sourcePDFDoc,
+    sourcePDFDoc.getPageIndices(),
+  );
+  pdfDoc.insertPage(5, copiedPages[0]);
+  functions.logger.log("Results page added to PDF");
+}
+
+/**
+ * This function creates the PDF.
+ * @param {string} userId The user id.
+ * @param {string} userName The user name.
+ * @param {string} date The date of the test.
+ */
+async function createPDF(
+  userId: string,
+  userName: string,
+  date: string,
+): Promise<PDFDocument> {
+  // Load the PDF file from Firebase Storage
+  const [pdfData, iconData, imageData] = await Promise.all([
+    bucket.file(filePath).download(),
+    bucket.file(iconPath).download(),
+    bucket.file(imagePath).download(),
+  ]);
+  functions.logger.log("All files downloaded");
+
+  const pdfBuffer = Buffer.from(pdfData[0]);
+  const iconBuffer = Uint8Array.from(iconData[0]);
+  const imageBuffer = Uint8Array.from(imageData[0]);
+
+  // Create a new PDF document
+  const pdfDoc = await PDFDocument.load(pdfBuffer);
+
+  // Create a cover page
+  await createCoverPage(
+    pdfDoc,
+    iconBuffer,
+    imageBuffer,
+    userName,
+    date,
+  );
+
+  // Add result PDF to the default PDF
+  await addResultPDF(pdfDoc, userId);
+
+  return pdfDoc;
+}
 
 /**
  * This function sends an email using Nodemailer.
+ * @param {PDFDocument} pdfDoc The PDF document.
  * @param {string} email The email address to send the email to.
- * @param {string} userId The user id.
- * @param {string} message The content of the email.
+ * @param {string} userName The user name.
  */
 async function sendEmail(
-  email: string, userId: string, message: string): Promise<void> {
-  // Create a PassThrough stream
-  const stream = new PassThrough();
-
-  // Download the PDF file from Cloud Storage and pipe it.
-  defaultResult.createReadStream().pipe(stream);
-
-  // Convert the PDF to a buffer
-  const pdfBuffer = await streamToBufferAsync(stream);
-
-  // Create a new PDF document from the existing buffer
-  const pdfDoc = await PDFDocument.load(pdfBuffer);
-  functions.logger.log("Default PDF loaded");
-
-  // Create a new PDF file from the new PDF document
-  const newPdfFile = bucket.file("development/UsersResults/"+userId+".pdf");
-
-  // Create a PassThrough stream for the new PDF
-  const newPdfStream = new PassThrough();
-  newPdfFile.createReadStream().pipe(newPdfStream);
-
-  // Convert the new PDF to a buffer
-  const newPdfBuffer = await streamToBufferAsync(newPdfStream);
-
-  // Load the new PDF document
-  const newPdfDoc = await PDFDocument.load(newPdfBuffer);
-  functions.logger.log("Result PDF loaded");
-
-  // Add the new PDF as a page to the existing PDF
-  const newPdfPages = newPdfDoc.getPages();
-  const newPdfPageIndices = Array.from(
-    {length: newPdfPages.length},
-    (_, i) => i,
-  );
-  const copiedPages = await pdfDoc.copyPages(newPdfDoc, newPdfPageIndices);
-
-  pdfDoc.insertPage(6, copiedPages[0]);
-
+  pdfDoc: PDFDocument,
+  email: string,
+  userName: string,
+): Promise<void> {
   // Serialize the updated PDF document to a buffer
   const updatedPdfBuffer = await pdfDoc.save();
 
@@ -107,8 +211,9 @@ async function sendEmail(
   const mailOptions = {
     from: "coachingtest23@gmail.com",
     to: email,
-    subject: "New Document Added",
-    text: message,
+    subject: "Vivir como Coach: Resultados del test",
+    // eslint-disable-next-line max-len
+    text: "Hola "+userName+",\n\nMuchas gracias por realizar el test. Adjunto encontrarás el informe con los resultados.\n\nUn saludo,\n\nEl equipo de Vivir como Coach",
     attachments: [
       {
         filename: "document.pdf",
@@ -118,6 +223,7 @@ async function sendEmail(
   };
 
   try {
+    functions.logger.log("Sending email");
     await transporter.sendMail(mailOptions);
   } catch (error) {
     console.error("Error sending email:", error);
@@ -139,8 +245,9 @@ exports.readTests = functions
     const userData = userReference.data();
     const userName = userData?.name;
     const userEmail = userData?.email;
+    const date = newValue?.coachingTestDate;
     functions.logger.log("User info", userName, userEmail);
 
-    const message = `Hi ${userName}, your test ${testId} has been read`;
-    await sendEmail(userEmail, userId, message);
+    const pdfDoc = await createPDF(userId, userName, date);
+    await sendEmail(pdfDoc, userEmail, userName);
   });
