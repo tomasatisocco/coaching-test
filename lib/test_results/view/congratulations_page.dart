2b834a02a457a5240144c20afdbae566953c540f
Tomas Tisocco <tomastisocco97@gmail.com>
2023-07-23 12:18:17 -0300

    Fix mobile view of congrats page

--- lib/test_results/view/congratulations_page.dart
+++ lib/test_results/view/congratulations_page.dart
@@ -21,55 +21,61 @@ class CongratulationsPage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              context.l10n.congratulations,
-              style: const TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              context.l10n.testSubmitted,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            const Icon(
-              Icons.check_circle_outline_rounded,
-              color: Colors.green,
-              size: 128,
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 400,
-              child: Text(
-                context.l10n.testSubmittedDescription,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  context.l10n.congratulations,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 32),
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset(
-                  'assets/images/live_as_coach_icon.png',
-                  width: 150,
+                const SizedBox(height: 16),
+                Text(
+                  context.l10n.testSubmitted,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
+                const SizedBox(height: 16),
+                const Icon(
+                  Icons.check_circle_outline_rounded,
+                  color: Colors.green,
+                  size: 128,
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 400,
+                  child: Text(
+                    context.l10n.testSubmittedDescription,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'assets/images/live_as_coach_icon.png',
+                      width: 150,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const NewTestButton(isMobile: true),
+              ],
             ),
-            const SizedBox(height: 16),
-            const NewTestButton(isMobile: true),
-          ],
+          ),
         ),
       ),
     );
